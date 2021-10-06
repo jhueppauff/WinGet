ccc7b4fa4d15ade3c2aa2d3f9879f885f9550406
Julian Hüppauff <julian@hueppauff.com>
2021-10-06 14:36:12 +0200

    Update developer.ps1

--- developer.ps1
+++ developer.ps1
@@ -1,8 +1,9 @@
 
-#### Install Colloboration
+#### Install Colloboration & Communication
 winget install -e Discord.Discord
 winget install -e SlackTechnologies.Slack
 #winget install -e Zoom.Zoom
+winget install -e Telegram.TelegramDesktop
 
 #### Install Office
 winget install -e Microsoft.Office
@@ -10,6 +11,8 @@ winget install -e Microsoft.Office
 #### Install Gaming
 #winget install -e Valve.Steam
 #winget install -e GOG.Galaxy
+#winget install -e Blizzard.BattleNet
+#winget install -e EpicGames.EpicGamesLauncher
 
 #### Install Utilities
 winget install -e DominikReichl.KeePass
@@ -20,6 +23,7 @@ winget install -e Microsoft.RemoteDesktopClient
 winget install -e PuTTY.PuTTY
 #winget install -e vim.vim
 #winget install -e TimKosse.FileZillaClientNightly
+winget install -e OpenVPNTechnologies.OpenVPNConnect
 
 #### Install Multimedia
 winget install -e Spotify.Spotify
@@ -33,6 +37,8 @@ winget install -e Microsoft.dotnet
 winget install -e Python.Python.3
 winget install -e Docker.DockerDesktop
 winget install -e OpenJS.NodeJS
+#winget install -e Kitware.CMake
+winget install -e Postman.Postman
 
 ##### Install Azure Tools
 winget install -e Microsoft.AzureStorageExplorer
@@ -45,6 +51,9 @@ winget install -e Microsoft.Bicep
 winget install -e Microsoft.AzureCLI
 # winget install -e Microsoft.ServiceFabricRuntime
 winget install -e Microsoft.AzureCLI
+
+#### Install Microsoft365 Suite
+#winget install -e Microsoft.PowerBI
 
 ### Install Visual Studio
 winget install Microsoft.VisualStudio.2019.Community --silent --override "--wait --quiet --addProductLang En-us --add Microsoft.VisualStudio.Workload.Azure"
