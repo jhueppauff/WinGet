ab19da234f57e312f8ab18bd835d4f202e4820f2
Julian Hüppauff <julian@hueppauff.com>
2022-02-15 09:57:44 +0100

    Update nano-azure-developer.ps1

--- nano-azure-developer.ps1
+++ nano-azure-developer.ps1
@@ -19,3 +19,25 @@ winget install -e Microsoft.AzureFunctionsCoreTools
 winget install -e Microsoft.Bicep
 winget install -e Microsoft.AzureCLI
 # winget install -e Microsoft.ServiceFabricRuntime
+
+#### VS Code Extension
+code --install-extension ms-vscode.csharp
+code --install-extension ms-vscode.azure-account
+code --install-extension ms-vscode.azurecli
+code --install-extension ms-azuretools.vscode-cosmosdb
+code --install-extension ms-azuretools.vscode-azurefunctions
+code --install-extension ms-azuredevops.azure-pipelines
+code --install-extension msazurermtools.azure-vscode-tools
+code --install-extension ms-azuretools.vscode-azureresourcegroups
+code --install-extension ms-azuretools.azurestorage
+code --install-extension ms-azuretools.vscode-bicep
+code --install-extension ms-dotnettools-csharp
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension ms-vscode.powershell
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+
+code --install-extension redhat.vscode-yaml
+
+#### Theme (optional)
+code --install-extension robbowen.synthwave-vscode
